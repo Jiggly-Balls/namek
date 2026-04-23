ae846dd742aee78c68a86bf722a99212895c2e18
Jiggly Balls <jigglyballs9000@gmail.com>
2026-04-23 20:32:32 +0530

    Added temporary command syncer

--- namek/core/bot.py
+++ namek/core/bot.py
@@ -3,8 +3,11 @@ from __future__ import annotations
 import logging
 from typing import TYPE_CHECKING
 
+import discord
 from disckit.utils import MentionTree
 from discord.ext import commands
+
+from namek.core.settings import SETTINGS
 
 if TYPE_CHECKING:
     from collections.abc import Collection
@@ -36,3 +39,19 @@ class Bot(commands.Bot):
             chunk_guilds_at_startup=False,
         )
         self.tree: MentionTree
+    
+    async def __temp_sync(self) -> None:  # pyright: ignore[reportUnusedFunction]
+        synced_global = await self.tree.sync()
+        synced_guild = await self.tree.sync(
+            guild=discord.Object(SETTINGS.DEV_GUILD_ID)
+        )
+
+        global_cmds = len(synced_global)
+        guild_cmds = len(synced_guild)
+        
+        _logger.info("Synced %s global commands.", global_cmds)
+        _logger.info("Synced %s guild commands.", guild_cmds)
+
+    async def setup_hook(self) -> None:
+        # await self.__temp_sync()
+        ...
