f86ddba4280572dc50a67d8ea5551e088576e2ee
Jiggly Balls <jigglyballs9000@gmail.com>
2026-05-08 15:50:52 +0530

    Added disckit extensions

--- namek/__main__.py
+++ namek/__main__.py
@@ -3,14 +3,22 @@ from __future__ import annotations
 import asyncio
 import logging
 import os
+from typing import TYPE_CHECKING
 
+import disckit
 import discord
 import wavelink
+from disckit import UtilConfig
+from disckit.cogs import dis_load_extension
 from discord.ext import commands
 from discord.utils import setup_logging
 
 from namek.core import Bot
 from namek.core.settings import BASE_DIR, COG_DIRECTORIES, SETTINGS
+
+if TYPE_CHECKING:
+    from typing import Any
+
 
 logging.basicConfig(
     filename="bot.log",
@@ -28,12 +36,30 @@ _logger: logging.Logger = logging.getLogger(__name__)
 async def main() -> None:
     intents = discord.Intents(guilds=True, members=True, voice_states=True)
 
+    UtilConfig.BUG_REPORT_CHANNEL = SETTINGS.BUG_REPORT_CHANNEL_ID
+    UtilConfig.STATUS_FUNC = (status_handler, ())
+    UtilConfig.STATUS_TYPE = discord.ActivityType.listening
+    UtilConfig.STATUS_COOLDOWN = 600
+
     try:
         bot = Bot(intents=intents, owner_ids=SETTINGS.OWNER_IDS)
+
         await load_extensions(bot)
+        await dis_load_extension(
+            bot,
+            disckit.CogEnum.ERROR_HANDLER,
+            disckit.CogEnum.STATUS_HANDLER,
+            disckit.CogEnum.HELP_COG,
+        )
+
         await bot.start(SETTINGS.BOT_TOKEN.get_secret_value())
     finally:
         await wavelink.Pool.close()
+
+
+async def status_handler(bot: Bot, *args: Any) -> tuple[str, ...]:
+    # Prefixed by "Listening to" from the activity type.
+    return ("Listening to humans.", "Listening to your horrible music taste.")
 
 
 async def load_extensions(bot: Bot) -> None:
