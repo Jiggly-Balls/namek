86bd7322a945c543134a71bb557ef025502fc4b2
Jiggly Balls <jigglyballs9000@gmail.com>
2026-05-10 01:12:23 +0530

    Added _vc_check & play command

--- namek/cogs/commands/music.py
+++ namek/cogs/commands/music.py
@@ -1,11 +1,15 @@
 import logging
+from typing import cast
+from urllib.parse import urlparse
 
 import discord
+import wavelink
 from disckit.cogs import BaseCog
 from disckit.utils import ErrorEmbed, SuccessEmbed
 from discord import Interaction, app_commands
 
 from namek.core import Bot
+from namek.core.utils import safe_defer
 
 _logger = logging.getLogger(__name__)
 
@@ -36,8 +40,7 @@ class MusicCog(BaseCog, name="Music Cog"):
         super().__init__(logger=_logger)
         self.bot: Bot = bot
 
-    @music_commands.command()
-    async def connect(self, interaction: Interaction[Bot]) -> None:
+    async def _vc_check(self, interaction: Interaction[Bot]) -> bool:
         assert interaction.guild
         assert isinstance(interaction.user, discord.Member)
 
@@ -47,11 +50,23 @@ class MusicCog(BaseCog, name="Music Cog"):
                     "You need to be within a voice channel to use this command."
                 )
             )
+            return False
+        return True
+
+    @music_commands.command()
+    async def connect(self, interaction: Interaction[Bot]) -> None:
+        assert isinstance(interaction.user, discord.Member)
+        assert interaction.guild
+        assert interaction.user.voice
+        assert interaction.user.voice.channel
+
+        in_vc = await self._vc_check(interaction)
+        if not in_vc:
             return
 
         await interaction.response.defer()
 
-        channel_name = interaction.user.voice.channel.name
+        channel_name: str = interaction.user.voice.channel.name
         await interaction.user.voice.channel.connect()
         await interaction.followup.send(
             embed=SuccessEmbed(
@@ -61,20 +76,19 @@ class MusicCog(BaseCog, name="Music Cog"):
 
     @music_commands.command()
     async def disconnect(self, interaction: Interaction[Bot]) -> None:
-        assert interaction.guild
         assert isinstance(interaction.user, discord.Member)
+        assert interaction.guild
+        assert interaction.user.voice
+        assert interaction.user.voice.channel
 
         if interaction.guild.voice_client is None:
             await interaction.response.send_message(
                 embed=ErrorEmbed("I'm not in a voice channel to disconnect.")
             )
             return
-        if not (interaction.user.voice and interaction.user.voice.channel):
-            await interaction.response.send_message(
-                embed=ErrorEmbed(
-                    "You need to be within a voice channel to use this command."
-                )
-            )
+
+        in_vc = await self._vc_check(interaction)
+        if not in_vc:
             return
 
         await interaction.response.defer()
@@ -89,8 +103,74 @@ class MusicCog(BaseCog, name="Music Cog"):
 
     @music_commands.command()
     async def play(self, interaction: Interaction[Bot], query: str) -> None:
-        assert interaction.guild
         assert isinstance(interaction.user, discord.Member)
+        assert interaction.guild
+
+        if interaction.guild.voice_client is None:
+            in_vc = await self._vc_check(interaction)
+            if not in_vc:
+                return
+
+            await interaction.response.defer()
+
+            assert interaction.user.voice
+            assert interaction.user.voice.channel
+
+            channel_name: str = interaction.user.voice.channel.name
+            await interaction.user.voice.channel.connect(cls=wavelink.Player)
+            await interaction.followup.send(
+                embed=SuccessEmbed(
+                    f"Successfully joined `{channel_name}` voice channel."
+                )
+            )
+        
+        await safe_defer(interaction)
+
+        player: wavelink.Player = cast(
+            "wavelink.Player", interaction.guild.voice_client
+        )
+
+        # Turn on AutoPlay to enabled mode.
+        # enabled = AutoPlay will play songs for us and fetch recommendations...
+        # partial = AutoPlay will play songs for us, but WILL NOT fetch recommendations...
+        # disabled = AutoPlay will do nothing...
+        player.autoplay = wavelink.AutoPlayMode.enabled
+
+        # Lock the player to this channel...
+        if not hasattr(player, "home"):
+            player.home = interaction.channel
+        elif player.home != interaction.channel:
+            await interaction.followup.send(
+                f"You can only play songs in {player.home.mention}, as the player has already started there."
+            )
+            return
+
+        # This will handle fetching Tracks and Playlists...
+        # Seed the doc strings for more information on this method...
+        # If spotify is enabled via LavaSrc, this will automatically fetch Spotify tracks if you pass a URL...
+        # Defaults to YouTube for non URL based queries...
+
+        tracks: wavelink.Search = await wavelink.Playable.search(query)
+        if not tracks:
+            await interaction.followup.send(
+                f"{interaction.user.mention} - Could not find any tracks with that query. Please try again."
+            )
+            return
+
+        if isinstance(tracks, wavelink.Playlist):
+            # tracks is a playlist...
+            added: int = await player.queue.put_wait(tracks)
+            await interaction.followup.send(
+                f"Added the playlist **`{tracks.name}`** ({added} songs) to the queue."
+            )
+        else:
+            track: wavelink.Playable = tracks[0]
+            await player.queue.put_wait(track)
+            await interaction.followup.send(f"Added **`{track}`** to the queue.")
+
+        if not player.playing:
+            # Play now since we aren't playing anything...
+            await player.play(player.queue.get(), volume=30)
 
 
 async def setup(bot: Bot) -> None:
