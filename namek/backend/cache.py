daeeb6747fe462fc187862319988cd2aacde5094
Jiggly Balls <jigglyballs9000@gmail.com>
2026-05-11 18:25:38 +0530

    Made Cache class into singleton

--- namek/backend/cache.py
+++ namek/backend/cache.py
@@ -1,30 +1,35 @@
 from __future__ import annotations
 
+from dataclasses import dataclass, field
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import ClassVar
- 
     from wavelink import Player
 
     from namek.utils.extras import VCState
 
 
-__all__ = ("Cache",)
+__all__ = ("CACHE",)
 
 
-class Cache:
-    """
-    The bot's shared external cache.
-
-    These attributes can be accessed globally but should be used sparingly,
-    as there is no explicit way to track their usage across the codebase.
-    This cache is only used when it is not feasible to pass data locally.
+@dataclass(slots=True)
+class _Cache:
+    """A singleton for the bot's shared external cache.
 
     Attributes
     ----------
-    vc_states : dict of int to tuple of (BaseView, Message)
-        A dictionary mapping guild IDs to their respective voice channel states.
+    vc_states : dict[Player, VCState]
+        | A dictionary mapping the player to it's VC state composing of the channel and
+        | message objects.
     """
 
-    vc_states: ClassVar[dict[Player, VCState]] = {}
+    vc_states: dict[Player, VCState] = field(default_factory=dict)
+
+    def delete_vc_state(self, player: Player) -> None:
+        try:
+            del self.vc_states[player]
+        except KeyError:
+            pass
+
+
+CACHE: _Cache = _Cache()
