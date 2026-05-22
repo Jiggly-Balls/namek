b430109e765cbf982e5139da0e65aee0f28cfd65
Jiggly Balls <jigglyballs9000@gmail.com>
2026-05-22 15:55:07 +0530

    Added embed types

---
+++ namek/utils/embeds.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from discord import Embed
+
+from namek.core.settings import ERROR_COLOUR, MAIN_COLOUR, SUCCESS_COLOUR
+
+if TYPE_CHECKING:
+    from typing import Any
+
+    from discord import Embed
+
+
+__all__ = ("MainEmbed", "ErrorEmbed", "SuccessEmbed")
+
+
+class MainEmbed(Embed):
+    def __init__(
+        self,
+        *,
+        title: None | str = None,
+        description: None | str = None,
+        url: None | str = None,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(
+            colour=MAIN_COLOUR,
+            title=title,
+            url=url,
+            description=description,
+            **kwargs,
+        )
+
+
+class ErrorEmbed(Embed):
+    def __init__(
+        self,
+        *,
+        title: None | str = None,
+        description: None | str = None,
+        url: None | str = None,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(
+            colour=ERROR_COLOUR,
+            title=title,
+            url=url,
+            description=description,
+            **kwargs,
+        )
+
+
+class SuccessEmbed(Embed):
+    def __init__(
+        self,
+        *,
+        title: None | str = None,
+        description: None | str = None,
+        url: None | str = None,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(
+            colour=SUCCESS_COLOUR,
+            title=title,
+            url=url,
+            description=description,
+            **kwargs,
+        )
