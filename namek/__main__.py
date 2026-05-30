6fa84b41a4d793a79a6cba529846fe3c67b46f2a
Jiggly Balls <jigglyballs9000@gmail.com>
2026-05-30 17:24:57 +0530

    Remove disckit

--- namek/__main__.py
+++ namek/__main__.py
@@ -3,11 +3,8 @@ from __future__ import annotations
 import asyncio
 import logging
 
-import disckit
 import discord
 import wavelink
-from disckit import UtilConfig
-from disckit.cogs import dis_load_extension
 from discord.utils import setup_logging
 
 from namek.core import Bot
@@ -28,16 +25,8 @@ _logger: logging.Logger = logging.getLogger(__name__)
 async def main() -> None:
     intents = discord.Intents(guilds=True, members=True, voice_states=True)
 
-    UtilConfig.BUG_REPORT_CHANNEL = SETTINGS.BUG_REPORT_CHANNEL_ID
-
     try:
         bot = Bot(intents=intents, owner_ids=SETTINGS.OWNER_IDS)
-
-        await dis_load_extension(
-            bot,
-            disckit.CogEnum.ERROR_HANDLER,
-        )
-
         await bot.start(SETTINGS.BOT_TOKEN.get_secret_value())
     finally:
         await wavelink.Pool.close()
