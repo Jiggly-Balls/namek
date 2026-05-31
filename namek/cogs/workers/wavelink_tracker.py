56baf01c9bdb8d146d824ef0ddcb2d622c519e67
Jiggly Balls <jigglyballs9000@gmail.com>
2026-05-31 16:27:55 +0530

    Added track end listener

--- namek/cogs/workers/wavelink_tracker.py
+++ namek/cogs/workers/wavelink_tracker.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import logging
 from typing import TYPE_CHECKING
 
+import wavelink
 from discord.ext import commands
 
 from namek.backend.cache import CACHE
@@ -55,7 +56,7 @@ class WavelinkTracker(
     async def on_wavelink_track_start(
         self, payload: TrackStartEventPayload
     ) -> None:
-        if not payload.player:
+        if payload.player is None:
             return
 
         vc_state = CACHE.vc_states.get(payload.player)
@@ -75,7 +76,53 @@ class WavelinkTracker(
     @commands.Cog.listener()
     async def on_wavelink_track_end(
         self, payload: TrackEndEventPayload
-    ) -> None: ...
+    ) -> None:
+        if payload.player is None:
+            return
+
+        vc_state = CACHE.vc_states.get(payload.player)
+        if not vc_state:
+            return
+
+        if not payload.player.queue and not payload.player.auto_queue:
+            post_text = (
+                "You can continue adding more tracks queue or "
+                "listen to our fine tuned auto recommendation."
+            )
+            if payload.player.autoplay is wavelink.AutoPlayMode.disabled:
+                post_text = (
+                    "You can continue listening to more songs by adding tracks to the queue. "
+                    "My autoplay is disabled. "
+                    "I will leave the voice channel in a few minutes if no songs are being played."
+                )
+
+            track = (
+                f"[{payload.track.title}]({payload.track.uri})"
+                if payload.track.uri
+                else f"**{payload.track.title}**"
+            )
+            artist = (
+                f"[{payload.track.author}]({payload.track.artist.url})"
+                if payload.track.artist.url
+                else f"**{payload.track.author}**"
+            )
+
+            track_end_embed = MainEmbed(
+                title="Finished playing your queue",
+                description="All tracks have finished playing from this queue."
+                f"\n{post_text}",
+            )
+            post_end_embed = (
+                MainEmbed()
+                .add_field(
+                    name="Last played track", value=f"{track}\n\n\\- {artist}"
+                )
+                .set_thumbnail(url=payload.track.artwork)
+            )
+
+            await vc_state.channel.send(
+                embeds=[track_end_embed, post_end_embed]
+            )
 
 
 async def setup(bot: Bot) -> None:
