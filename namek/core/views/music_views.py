08b0af7a4eb4e348739af24dd27af0875e0c61cf
Jiggly Balls <jigglyballs9000@gmail.com>
2026-05-31 17:10:10 +0530

    Formatting

--- namek/core/views/music_views.py
+++ namek/core/views/music_views.py
@@ -127,9 +127,7 @@ class PlayView(BaseView):
         await self.player.pause(self.is_pause)
 
         if interaction.message:
-            button.emoji = (
-                f"{EMOJIS.PAUSE}" if self.is_pause else f"{EMOJIS.PLAY}"
-            )
+            button.emoji = EMOJIS.PAUSE if self.is_pause else EMOJIS.PLAY
             await interaction.followup.edit_message(
                 interaction.message.id, view=self
             )
