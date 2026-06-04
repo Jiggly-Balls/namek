8b5cc002c9068b8be72412fcf519c5ff7b962cb0
Jiggly Balls <jigglyballs9000@gmail.com>
2026-06-04 23:09:42 +0530

    Added view kward to dataclass

--- namek/utils/extras.py
+++ namek/utils/extras.py
@@ -6,6 +6,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from discord import Message, TextChannel
 
+    from namek.core.views.music_views import PlayLayoutView
 
 __all__ = ("VCState",)
 
@@ -13,4 +14,5 @@ __all__ = ("VCState",)
 @dataclass(slots=True, kw_only=True)
 class VCState:
     channel: TextChannel
+    view: PlayLayoutView
     message: None | Message = None
