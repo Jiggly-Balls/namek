dfb70118af26c4f78984bd7c9d201b2e36e44d81
Jiggly Balls <jigglyballs9000@gmail.com>
2026-06-10 20:06:22 +0530

    Temporarily remove view from VCState dataclass

--- namek/utils/extras.py
+++ namek/utils/extras.py
@@ -6,13 +6,10 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from discord import Message, TextChannel
 
-    from namek.core.views.music_views import PlayLayoutView
-
 __all__ = ("VCState",)
 
 
 @dataclass(slots=True, kw_only=True)
 class VCState:
     channel: TextChannel
-    view: PlayLayoutView
     message: None | Message = None
