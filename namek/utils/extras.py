1a91002c2886e4e4dbad03df3c86969c872c9928
Jiggly Balls <jigglyballs9000@gmail.com>
2026-06-11 00:59:29 +0530

    Added view attribute to VCState

--- namek/utils/extras.py
+++ namek/utils/extras.py
@@ -3,8 +3,12 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
+from discord.utils import MISSING
+
 if TYPE_CHECKING:
     from discord import Message, TextChannel
+
+    from namek.core.views.music_views import PlayLayoutView
 
 __all__ = ("VCState",)
 
@@ -13,3 +17,4 @@ __all__ = ("VCState",)
 class VCState:
     channel: TextChannel
     message: None | Message = None
+    view: PlayLayoutView = MISSING
